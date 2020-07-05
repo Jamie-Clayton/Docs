4afc191fea8a752194743c1118294078f9f3b336
Jamie Clayton <jamie@jenasys.com>
2020-07-06 09:08:32 +1000

    Add Chocolatey installation examples

---
+++ devops/Powershell/Choco-Install-Npm.ps1
@@ -0,0 +1,17 @@
+﻿choco install nodejs.install
+# Re-load powershell environment before running the rest of these scripts.
+
+# Display Node.js edition
+node -v
+
+# Display Node Package Manager edition
+npm -v
+
+# Install Angular.io
+npm install -g @angular/cli
+
+# Navigate to source folder
+cd C:\Users\jamie.clayton\Documents\Source
+
+ng new spike-ng-refresher
+ng serve
---
+++ devops/Powershell/Choco-InstallTerraform.ps1
@@ -0,0 +1,16 @@
+﻿# Install Chocolately via https://chocolatey.org/
+choco feature enable -n allowGlobalConfirmation
+
+# Install infrastructure as code tool - Terraform.io and Packer.io
+choco install terraform --version 0.11.8 --yes
+choco install packer --yes --force
+
+# Confirm Installation
+terraform -version
+
+# Navigated to Terraform Git clones
+cd C:\Users\jamie.clayton\Documents\Terraform\
+cd aws-287-Dominos-Supply_Chain
+
+# Open Microsoft Visual Code with the active folder loaded
+Code folder .
