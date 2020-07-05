858198a993987a1c5884d6842ee6948461578455
Jamie Clayton <jamie@jenasys.com>
2020-07-06 09:08:09 +1000

    Add Powershell automation examples
    * Azure Resource Management

---
+++ devops/Powershell/Azure_FireWallQueries.ps1
@@ -0,0 +1,13 @@
+﻿# Install the Resource Graph module from PowerShell Gallery
+Install-Module -Name Az.ResourceGraph
+
+# Get a list of commands for the imported Az.ResourceGraph module
+Get-Command -Module 'Az.ResourceGraph' -CommandType 'Cmdlet'
+Connect-AzAccount
+Search-AzGraph -Query 'Resources | project name, type | limit 5'
+
+az accounts list
+# Open Azure Account Subscription
+az account set -s {Subscription GUID goes here}
+az sql server list
+az sql server firewall-rule list -g ResourceGroupNameGoesHere -s SqlServerInstanceNameGoesHere | Select-Object -Property name,endIpAddress | ForEach-Object -Process {$_}
