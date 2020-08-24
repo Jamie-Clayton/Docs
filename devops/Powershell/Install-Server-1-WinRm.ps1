e3c09497a760ba73da96a776ed29d3b1d9d3f270
Jamie Clayton <jamie@jenasys.com>
2020-08-25 08:50:49 +1000

    Fix script spacing

--- devops/Powershell/Install-Server-1-WinRm.ps1
+++ devops/Powershell/Install-Server-1-WinRm.ps1
@@ -9,9 +9,11 @@ Enable-PsRemoting -SkipNetworkProfileCheck
 
 # 2. Add a new firewall rule for HTTPS
 netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5986
+
 # 3. Create a self signed cert - https://docs.microsoft.com/en-us/powershell/module/pkiclient/New-SelfSignedCertificate?view=winserver2012r2-ps
 $hostName = [System.Net.Dns]::GetHostByName($env:computerName).HostName
 New-SelfSignedCertificate -DnsName $hostName -CertStoreLocation Cert:\LocalMachine\My
+
 # 4. Register WinRM to use the self signed cert.
 WinRM create winrm/config/Listener?Address=*+Transport=HTTPS '@{Hostname="<YOUR_DNS_NAME>"; CertificateThumbprint="<COPIED_CERTIFICATE_THUMBPRINT>"}'
 
