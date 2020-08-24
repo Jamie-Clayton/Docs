0e9f07d040b1c019d7d6c9b2670cb2bf685e2b4e
Jamie Clayton <jamie@jenasys.com>
2020-08-24 20:26:30 +1000

    Add powershell URI space escaping example

---
+++ devops/Powershell/UriSpaceEscaping.ps1
@@ -0,0 +1,9 @@
+﻿[uri]::EscapeDataString("http://jamie clayton.com")
+
+
+[uri]::EscapeUriString("https://jamie clayton secure.com")
+
+# Test the code with variables
+$applicationName = "http://Jamie Clayton Again.com"
+$friendlyName = [uri]::EscapeUriString("$applicationName")
+$friendlyName
